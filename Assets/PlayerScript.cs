2158a7e3d4290c1a992b5dedf5c6561d884dc74d
DarkLoard27 <atharva.kharapkar@gmail.com>
2022-01-01 13:36:30 +0530

    player movement fixed

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField] private float distanceToMove;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float cameraMoveSpeed;
-    public Collider2D moveUp, moveDown, moveRight, moveLeft,wall;
 
     [SerializeField] private GameObject torch;
     public GameObject camera;
     private Vector3 cameraOffset;
     enum Direction { centre, up,right ,down ,left};
+    private Vector3 playerDirection;
 
     Direction torchDirection;
     private bool moveToPoint = false;
     private Vector3 endPosition;
+
+    //tilemaps
+    [SerializeField] private Tilemap groundTimeMap;
+    [SerializeField] private Tilemap wallTileMap;
+
+    
     void Start()
     {
         endPosition = transform.position;
@@ -26,43 +33,84 @@ public class PlayerScript : MonoBehaviour
 
         camera = GameObject.Find("Main Camera");
         cameraOffset.z = camera.transform.position.z;
+
+        Debug.Log("wall" + wallTileMap.size);
     }
 
     void FixedUpdate()
     {
-        if (moveToPoint)
-        {
-
-            transform.position = Vector3.MoveTowards(transform.position, endPosition, moveSpeed * Time.deltaTime);
-        }
+        Move();
     }
 
     private void LateUpdate()
     {
-        camera.transform.position = Vector3.MoveTowards(camera.transform.position, endPosition+cameraOffset, cameraMoveSpeed * Time.deltaTime);
+        if (moveToPoint && canMove(playerDirection))
+        {
+            camera.transform.position = Vector3.MoveTowards(camera.transform.position, endPosition + cameraOffset, cameraMoveSpeed * Time.deltaTime);
+        }
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) && !moveLeft.IsTouching(wall)) //Left
+        playerMove();
+    }
+
+    private bool canMove(Vector3 direction)
+    {
+        Vector3Int gridPosition = groundTimeMap.WorldToCell(direction);
+        if (!groundTimeMap.HasTile(gridPosition)|| wallTileMap.HasTile(gridPosition))
+            return false;
+        return true;
+    }
+    private void Move()
+    {
+        if (moveToPoint)
         {
-            Debug.Log("left coll" + moveLeft.IsTouching(wall));
-            endPosition = new Vector3(endPosition.x - distanceToMove, endPosition.y, endPosition.z);
-            moveToPoint = true;
+            transform.position = Vector3.MoveTowards(transform.position, endPosition, moveSpeed * Time.deltaTime);
+            if (transform.position.Equals(endPosition))
+                moveToPoint = false;
         }
-        if (Input.GetKeyDown(KeyCode.D) && !moveRight.IsTouching(wall)) //Right
+    }
+    private void playerMove()
+    {
+        if (!moveToPoint)
         {
-            endPosition = new Vector3(endPosition.x + distanceToMove, endPosition.y, endPosition.z);
-            moveToPoint = true;
-        }
-        if (Input.GetKeyDown(KeyCode.W)  )//Up
-        {
-            endPosition = new Vector3(endPosition.x, endPosition.y + distanceToMove, endPosition.z);
-            moveToPoint = true;
-        }
-        if (Input.GetKeyDown(KeyCode.S)) //Down
-        {
-            endPosition = new Vector3(endPosition.x, endPosition.y - distanceToMove, endPosition.z);
-            moveToPoint = true;
+            if (Input.GetKeyDown(KeyCode.A)) //Left
+            {
+                playerDirection = new Vector3(endPosition.x - distanceToMove, endPosition.y, endPosition.z);
+                if (canMove(playerDirection))
+                {
+                    endPosition = playerDirection;
+                    moveToPoint = true;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.D)) //Right
+            {
+                playerDirection = new Vector3(endPosition.x + distanceToMove, endPosition.y, endPosition.z);
+                if (canMove(playerDirection))
+                {
+                    endPosition = playerDirection;
+                    moveToPoint = true;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.W))//Up
+            {
+                playerDirection = new Vector3(endPosition.x, endPosition.y + distanceToMove, endPosition.z);
+                if (canMove(playerDirection))
+                {
+                    endPosition = playerDirection;
+                    moveToPoint = true;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.S)) //Down
+            {
+                playerDirection = new Vector3(endPosition.x, endPosition.y - distanceToMove, endPosition.z);
+                if (canMove(playerDirection))
+                {
+                    endPosition = playerDirection;
+                    moveToPoint = true;
+                }
+            }
+
         }
     }
 }
